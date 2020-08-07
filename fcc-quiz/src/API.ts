249b5cdd86bb12c4869ffd5de097e160e571d946
Vincent Nguyen <vpvnguyen@gmail.com>
2020-08-06 22:20:41 -0700

    define and return questions and answers

--- fcc-quiz/src/API.ts
+++ fcc-quiz/src/API.ts
@@ -1,3 +1,18 @@
+import { shuffleArray } from "./utils";
+
+// define type for each question
+export type Questions = {
+  category: string;
+  correct_answer: string;
+  difficulty: string;
+  incorrect_answers: string[];
+  question: string;
+  type: string;
+};
+
+// have both correct and incorrect answers in the same array
+export type QuestionState = Questions & { answers: string[] };
+
 export enum Difficulty {
   EASY = "easy",
   MEDIUM = "medium",
@@ -10,5 +25,11 @@ export const fetchQuizQuestions = async (
 ) => {
   const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
   const data = await (await fetch(endpoint)).json();
-  console.log(data);
+  return data.results.map((question: Questions) => ({
+    ...question,
+    answers: shuffleArray([
+      ...question.incorrect_answers,
+      question.correct_answer,
+    ]),
+  }));
 };
